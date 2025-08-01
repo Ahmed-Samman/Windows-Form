19b52c8bca695eb0a6b495941b6b49ce53d600e6
Samman <asamman560@gmail.com>
2025-08-01 18:03:02 +0300

    ADD "List View"

---
+++ List View/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace List_View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAddPerson = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnFillRandom = new System.Windows.Forms.Button();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbtnDetails = new System.Windows.Forms.RadioButton();
+            this.rbtnTitle = new System.Windows.Forms.RadioButton();
+            this.rbtnSmallIcon = new System.Windows.Forms.RadioButton();
+            this.rbtnList = new System.Windows.Forms.RadioButton();
+            this.rbtnLargeIcon = new System.Windows.Forms.RadioButton();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtPhoneNum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAddPerson
+            // 
+            this.btnAddPerson.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnAddPerson.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddPerson.Location = new System.Drawing.Point(365, 92);
+            this.btnAddPerson.Name = "btnAddPerson";
+            this.btnAddPerson.Size = new System.Drawing.Size(204, 45);
+            this.btnAddPerson.TabIndex = 0;
+            this.btnAddPerson.Text = "Add";
+            this.btnAddPerson.UseVisualStyleBackColor = true;
+            this.btnAddPerson.Click += new System.EventHandler(this.btnAddPerson_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(589, 92);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(204, 45);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnFillRandom
+            // 
+            this.btnFillRandom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnFillRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFillRandom.Location = new System.Drawing.Point(812, 92);
+            this.btnFillRandom.Name = "btnFillRandom";
+            this.btnFillRandom.Size = new System.Drawing.Size(204, 45);
+            this.btnFillRandom.TabIndex = 2;
+            this.btnFillRandom.Text = "Fill Random";
+            this.btnFillRandom.UseVisualStyleBackColor = true;
+            this.btnFillRandom.Click += new System.EventHandler(this.btnFillRandom_Click);
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnMale.Location = new System.Drawing.Point(19, 37);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(80, 29);
+            this.rbtnMale.TabIndex = 3;
+            this.rbtnMale.TabStop = true;
+            this.rbtnMale.Text = "Male";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnFemale.Location = new System.Drawing.Point(176, 37);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(102, 29);
+            this.rbtnFemale.TabIndex = 4;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "Female";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1033, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "View";
+            // 
+            // rbtnDetails
+            // 
+            this.rbtnDetails.AutoSize = true;
+            this.rbtnDetails.Checked = true;
+            this.rbtnDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnDetails.Location = new System.Drawing.Point(1049, 80);
+            this.rbtnDetails.Name = "rbtnDetails";
+            this.rbtnDetails.Size = new System.Drawing.Size(90, 26);
+            this.rbtnDetails.TabIndex = 0;
+            this.rbtnDetails.TabStop = true;
+            this.rbtnDetails.Text = "Details";
+            this.rbtnDetails.UseVisualStyleBackColor = true;
+            this.rbtnDetails.CheckedChanged += new System.EventHandler(this.rbtnDetails_CheckedChanged);
+            // 
+            // rbtnTitle
+            // 
+            this.rbtnTitle.AutoSize = true;
+            this.rbtnTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnTitle.Location = new System.Drawing.Point(1049, 173);
+            this.rbtnTitle.Name = "rbtnTitle";
+            this.rbtnTitle.Size = new System.Drawing.Size(70, 26);
+            this.rbtnTitle.TabIndex = 8;
+            this.rbtnTitle.Text = "Title";
+            this.rbtnTitle.UseVisualStyleBackColor = true;
+            this.rbtnTitle.CheckedChanged += new System.EventHandler(this.rbtnTitle_CheckedChanged);
+            // 
+            // rbtnSmallIcon
+            // 
+            this.rbtnSmallIcon.AutoSize = true;
+            this.rbtnSmallIcon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnSmallIcon.Location = new System.Drawing.Point(1049, 128);
+            this.rbtnSmallIcon.Name = "rbtnSmallIcon";
+            this.rbtnSmallIcon.Size = new System.Drawing.Size(117, 26);
+            this.rbtnSmallIcon.TabIndex = 9;
+            this.rbtnSmallIcon.Text = "Small Icon";
+            this.rbtnSmallIcon.UseVisualStyleBackColor = true;
+            this.rbtnSmallIcon.CheckedChanged += new System.EventHandler(this.rbtnSmallIcon_CheckedChanged);
+            // 
+            // rbtnList
+            // 
+            this.rbtnList.AutoSize = true;
+            this.rbtnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnList.Location = new System.Drawing.Point(1207, 128);
+            this.rbtnList.Name = "rbtnList";
+            this.rbtnList.Size = new System.Drawing.Size(63, 26);
+            this.rbtnList.TabIndex = 10;
+            this.rbtnList.Text = "List";
+            this.rbtnList.UseVisualStyleBackColor = true;
+            this.rbtnList.CheckedChanged += new System.EventHandler(this.rbtnList_CheckedChanged);
+            // 
+            // rbtnLargeIcon
+            // 
+            this.rbtnLargeIcon.AutoSize = true;
+            this.rbtnLargeIcon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnLargeIcon.Location = new System.Drawing.Point(1207, 80);
+            this.rbtnLargeIcon.Name = "rbtnLargeIcon";
+            this.rbtnLargeIcon.Size = new System.Drawing.Size(119, 26);
+            this.rbtnLargeIcon.TabIndex = 11;
+            this.rbtnLargeIcon.Text = "Large Icon";
+            this.rbtnLargeIcon.UseVisualStyleBackColor = true;
+            this.rbtnLargeIcon.CheckedChanged += new System.EventHandler(this.rbtnLargeIcon_CheckedChanged);
+            // 
+            // txtID
+            // 
+            this.txtID.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtID.Location = new System.Drawing.Point(78, 24);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(115, 26);
+            this.txtID.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "ID:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(109, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(222, 26);
+            this.txtName.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnFemale);
+            this.groupBox1.Controls.Add(this.rbtnMale);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.groupBox1.Location = new System.Drawing.Point(37, 152);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 83);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "2533719.png");
+            this.imageList1.Images.SetKeyName(1, "girl_146005 (1).png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "2533719.png");
+            this.imageList2.Images.SetKeyName(1, "girl_146005 (1).png");
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(37, 250);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1388, 391);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 17;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Name";
+            this.columnHeader3.Width = 200;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Phone";
+            this.columnHeader1.Width = 150;
+            // 
+            // txtPhoneNum
+            // 
+            this.txtPhoneNum.Location = new System.Drawing.Point(109, 111);
+            this.txtPhoneNum.Name = "txtPhoneNum";
+            this.txtPhoneNum.Size = new System.Drawing.Size(222, 26);
+            this.txtPhoneNum.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 25);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Phone:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAddPerson;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1462, 653);
+            this.Controls.Add(this.txtPhoneNum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.rbtnLargeIcon);
+            this.Controls.Add(this.rbtnList);
+            this.Controls.Add(this.rbtnSmallIcon);
+            this.Controls.Add(this.rbtnTitle);
+            this.Controls.Add(this.rbtnDetails);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFillRandom);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAddPerson);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddPerson;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnFillRandom;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtnDetails;
+        private System.Windows.Forms.RadioButton rbtnTitle;
+        private System.Windows.Forms.RadioButton rbtnSmallIcon;
+        private System.Windows.Forms.RadioButton rbtnList;
+        private System.Windows.Forms.RadioButton rbtnLargeIcon;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox txtPhoneNum;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ List View/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddPerson_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtID.Text))
+                return;
+            
+            ListViewItem item = new ListViewItem(txtID.Text.Trim());
+
+            if (rbtnMale.Checked)
+                item.ImageIndex = 0;
+            else
+                item.ImageIndex = 1;
+            
+            item.SubItems.Add(txtName.Text.Trim());
+            item.SubItems.Add(txtPhoneNum.Text.Trim());
+            listView1.Items.Add(item);
+
+            txtID.Clear();
+            txtName.Clear();
+            txtPhoneNum.Clear();
+            txtID.Focus();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+            if (listView1.Items.Count == 0)
+                return;
+                       
+            int IndexToDelete = listView1.SelectedIndices.Count > 0 ? listView1.SelectedIndices[0] 
+                : listView1.Items.Count - 1; // Default to the last item if none selected
+
+            listView1.Items.RemoveAt(IndexToDelete);            
+        }
+
+        private void btnFillRandom_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                ListViewItem item = new ListViewItem(i.ToString());
+
+                if (i % 2 == 0)
+                    item.ImageIndex = 0;
+                else
+                    item.ImageIndex = 1;
+
+                item.SubItems.Add("Person " + i.ToString());
+                listView1.Items.Add(item);
+            }
+        }
+
+
+        private void rbtnDetails_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+        }
+        private void rbtnSmallIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+        private void rbtnTitle_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.Tile;
+        }
+        private void rbtnLargeIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+        private void rbtnList_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
