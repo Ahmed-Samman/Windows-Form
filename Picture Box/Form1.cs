2ae404ecb769a1d13a2d0e411d872c56d7590c8e
Samman <asamman560@gmail.com>
2025-08-01 18:08:46 +0300

    ADD "Picture Box"

---
+++ Picture Box/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Picture_Box.Properties;
+
+namespace Picture_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.aevkt99a;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.hwrxvts4;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"D:\C#\Windows Form\Picture Box\bgfvyboc.png");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new PictureBox().Image; // This for delete Picture
+
+        }
+    }
+}
